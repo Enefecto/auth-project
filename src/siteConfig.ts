dbdb3223ee41e10203c897314bec610b16ba46d1
Camilo Romero <camids980@gmail.com>
2024-09-23 21:20:55 -0300

    feat: NavBar y SEO agregado, chore: author y keywords agregados en siteConfig

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -1,4 +1,6 @@
 export const siteConfig = {
   title: "Auth Astro",
   description: "Astro App para crear una autenticación usando Firebase",
+  author: "Camilo Romero",
+  keywords: "Auth, Firebase, aprendiendo",
 };
