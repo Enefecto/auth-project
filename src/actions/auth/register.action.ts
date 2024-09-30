adfa876acc13e1099014ba85b109d7d3f7e640b5
Camilo Romero <camids980@gmail.com>
2024-09-30 19:59:02 -0300

    feat: regristar usuario, fix: .env ahora en la raiz del proyecto

--- src/actions/auth/register.action.ts
+++ src/actions/auth/register.action.ts
@@ -1,5 +1,7 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { createUserWithEmailAndPassword, type AuthError } from "firebase/auth";
+import { firebase } from "../../firebase/config";
 
 export const registerUser = defineAction({
   accept: "form",
@@ -14,11 +16,10 @@ export const registerUser = defineAction({
     { name, username, email, password, remember_me },
     { cookies }
   ) => {
-    console.log({ name, username, email, password, remember_me });
-
+    // Cookies
     if (remember_me) {
       cookies.set("email", email, {
-        expires: new Date(Date.now() + 86400000 /*1 Dia*/ * 365),
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), // 1 año,
         path: "/",
       });
     } else {
@@ -27,6 +28,29 @@ export const registerUser = defineAction({
       });
     }
 
-    return { ok: true, msg: "Usuario Creado" };
+    // Creación de usuario
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        firebase.auth,
+        email,
+        password
+      );
+
+      // Extrae sólo la información serializable
+      const user = userCredential.user;
+      return {
+        uid: user.uid,
+        email: user.email,
+        // Puedes agregar más campos si los necesitas
+      };
+    } catch (error) {
+      const firebaseError = error as AuthError;
+      if (firebaseError.code === "auth/email-already-in-use") {
+        throw new Error("El correo ya está en uso");
+      }
+
+      throw new Error("Auxilio! algo salió mal");
+    }
   },
 });
